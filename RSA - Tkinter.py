ff703de4c02cccfec4402dedcf0f1192faef3504
Tejaaswini Narendra <30696079+Tejaaswini@users.noreply.github.com>
2019-12-30 16:30:10 +0530

    Update RSA - Tkinter.py

--- RSA - Tkinter.py
+++ RSA - Tkinter.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[40]:
-
-
 from tkinter import *
 def euclid(a, b):
        
@@ -11,11 +8,6 @@ def euclid(a, b):
         return a
     else:
         return euclid(b, a % b)
- 
-
-
-# In[41]:
-
 
 def keys_generation(p, q):
 
@@ -40,11 +32,6 @@ def keys_generation(p, q):
     d=w
     return [(e, n), (d, n)]
 
-
-# In[48]:
-
-
-
 def rsa():
     global l2
     p= int(a.get())
@@ -64,56 +51,32 @@ def rsa():
     l2.config(text=crypted)
     return crypted**d % n
 
-
-# In[49]:
-
-
-
 def decryp():
     b=rsa()
     global l3
     original = b
     l3.config(text=original)
-    
-
-
-# In[62]:
-
 
 root = Tk()    
 root.title('RSA')   
-
 l = Label(root,text='Enter First Prime')
 l.pack() 
-
 a = Entry(root) 
 a.pack()
-
 l = Label(root,text='Enter Second Prime')
 l.pack()
-
 b = Entry(root) 
 b.pack()
-
 l = Label(root,text='Enter Message (only numbers)')
 l.pack()
-
 c = Entry(root)
 c.pack()
-
 f2=Frame()
 f2.pack(side=TOP)
 l2= Label(f2,text='Crypted Msg')
 l2.pack(side=LEFT)
-
 l3=Label(f2,text='Decrypted Msg')
 l3.pack(side=RIGHT)
-
-
-# In[63]:
-
-
-
 f1=Frame()
 f1.pack(side=TOP)
 button = Button(f1,text="Encrypt",command=rsa) 
@@ -123,10 +86,3 @@ button2.pack(side=LEFT)
 button3= Button(f1,text="More info",command=info)
 button3.pack(side=TOP)
 root.mainloop()
-
-
-# In[ ]:
-
-
-
-
